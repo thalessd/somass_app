6c97a6e1beadae414b71c767eb05a100922c0c15
Thales Carvalho <thalessd@live.com>
2020-09-20 20:16:23 -0300

    Adição de rota de teste do servidor

--- lib/app/shared/constants/constant.dart
+++ lib/app/shared/constants/constant.dart
@@ -1,4 +1,4 @@
 class Constant {
-  static const String SERVER_URL = "http://192.168.0.110:3000";
+  static const String SERVER_URL = "http://devdes.io:3001";
   static const int ESCORTS_MAX = 2;
 }
