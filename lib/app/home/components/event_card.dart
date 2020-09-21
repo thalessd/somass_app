629954fd436ed6b935cc2bd02a4aced838c45912
Thales Carvalho <thalessd@live.com>
2020-09-21 01:00:02 -0300

    Mudança nas cores dos cards

--- lib/app/home/components/event_card.dart
+++ lib/app/home/components/event_card.dart
@@ -11,70 +11,84 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final timeFormat = DateFormat("HH'h' mm");
 
     final dayBrl = Helper.dayOfWeekToBrlShort(event.dayOfWeek).toUpperCase();
     final time = timeFormat.format(event.date.add(Duration(hours: -3)));
 
-    final cardColor = event.hasPassed
-        ? Colors.grey[800]
-        : event.hasParticipation ? Colors.blueAccent : Colors.lightGreen[700];
+    var cardColor = event.hasPassed
+        ? Colors.grey[200]
+        : event.hasParticipation ? Colors.blue : Colors.green[700];
 
-    final cardBottomDescription = event.hasPassed ? "Indisponível" : event
-        .hasParticipation ? "Agendado" : "${event.occupiedVacancies} de ${event
-        .totalVacancies}";
+    var cardBottomDescription = event.hasPassed
+        ? "Indisponível"
+        : event.hasParticipation
+            ? "Agendado"
+            : "${event.occupiedVacancies} de ${event.totalVacancies}";
+
+    final textColor = event.hasPassed ? Colors.grey[400] : Colors.white;
+
+    if (event.occupiedVacancies == event.totalVacancies && !event.hasParticipation) {
+      cardBottomDescription = "Lotado";
+      cardColor = Colors.deepOrangeAccent;
+    }
 
     return Card(
-      elevation: 4,
+      elevation: event.hasPassed ? 0 : 4,
       margin: EdgeInsets.all(0.0),
       color: cardColor,
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(10))),
       child: InkWell(
-        onTap: event.hasPassed ? null : () { onOpenEvent(event); },
+        onTap: event.hasPassed
+            ? null
+            : () {
+                onOpenEvent(event);
+              },
         child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-          Expanded(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                dayBrl,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        dayBrl,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: textColor,
+                        ),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 1,
+                        margin: EdgeInsets.only(top: 4, bottom: 4),
+                        color: textColor,
+                      ),
+                      Text(
+                        time,
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: textColor),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                width: 60,
-                height: 1,
-                margin: EdgeInsets.only(top: 4, bottom: 4),
-                color: Colors.white,
-              ),
-              Text(
-                time,
-                style: TextStyle(
-                    fontSize: 18,
+                Text(
+                  cardBottomDescription,
+                  style: TextStyle(
+                    fontSize: 13,
                     fontWeight: FontWeight.w700,
-                    color: Colors.white),
-              ),
-            ],
-          ),
-        ),
-        Text(cardBottomDescription, style: TextStyle(
-        fontSize: 13,
-        fontWeight: FontWeight.w700,
-        color: Colors.white,
-        ),)
-      ],
-    )),
-    )
-    ,
+                    color: textColor,
+                  ),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
