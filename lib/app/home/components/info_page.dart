2590eda9c6f26b49311746b9c93881c5dae73294
Thales Carvalho <thalessd@live.com>
2020-09-21 08:54:47 -0300

    Modificações na tela de informações

--- lib/app/home/components/info_page.dart
+++ lib/app/home/components/info_page.dart
@@ -42,25 +42,33 @@ class InfoPage extends StatelessWidget {
             height: 12,
           ),
           ListTile(
-            onTap: () { launch("tel:89 99988 9460"); },
+            onTap: () {
+              launch("tel:89 99988 9460");
+            },
             leading: Icon(Icons.phone),
             title: Text("Telefone"),
             subtitle: Text("89 99988 9460"),
           ),
           ListTile(
-            onTap: () { launch("https://api.whatsapp.com/send?phone=5589999889460"); },
+            onTap: () {
+              launch("https://api.whatsapp.com/send?phone=5589999889460");
+            },
             leading: Icon(MdiIcons.whatsapp),
             title: Text("Whatsapp"),
             subtitle: Text("89 99988 9460"),
           ),
           ListTile(
-            onTap: () { launch("https://instagram.com/devdes.io"); },
+            onTap: () {
+              launch("https://instagram.com/devdes.io");
+            },
             leading: Icon(MdiIcons.instagram),
             title: Text("Instagram"),
             subtitle: Text("@devdes.io"),
           ),
           ListTile(
-            onTap: () { launch("https://devdes.io"); },
+            onTap: () {
+              launch("https://devdes.io");
+            },
             leading: Icon(MdiIcons.application),
             title: Text("Site"),
             subtitle: Text("www.devdes.io"),
@@ -76,12 +84,20 @@ class InfoPage extends StatelessWidget {
           ),
           Text(
             "Este aplicativo foi criado com o intuito"
-                " de ajudar, a todos que possuem um celular inteligente, consigam,"
-                " marcar o melhor horário e dia da semana para "
-                "participar da missa, eliminando a necessidade de entrar em "
-                "contato via mensagem de texto e afins.",
+            " de ajudar no periodo da pandemia, para que todos possam"
+            " marcar o melhor horário e dia da semana para "
+            "participar da missa, eliminando a necessidade de entrar em "
+            "contato via mensagem de texto e afins, assim respeitando as "
+            "medidas de distânciamento.",
             style: Theme.of(context).textTheme.bodyText1,
             textAlign: TextAlign.justify,
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+            "Obrigado por baixar nosso aplicativo!",
+            style: Theme.of(context).textTheme.subtitle1,
           ),
           SizedBox(
             height: 8,
